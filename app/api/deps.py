e6dcf3f0892e27d2117b4bf212892ccfb7e31d7d
andrey <defario777@mail.ru>
2026-04-22 09:47:51 +0500

    add deps

---
+++ app/api/deps.py
@@ -0,0 +1,19 @@
+from typing import Annotated, AsyncGenerator
+
+from fastapi import Depends
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.database import async_session_factory
+
+
+async def get_session() -> AsyncGenerator:
+    async with async_session_factory() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
