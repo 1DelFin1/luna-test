8acfb62d08f1aa731348bca493ef5b1df0b84049
andrey <defario777@mail.ru>
2026-04-22 14:43:46 +0500

    add starting in lifespan

--- app/main.py
+++ app/main.py
@@ -1,10 +1,28 @@
+import asyncio
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
+from faststream.rabbit import RabbitBroker
 
 from app.api.routers.payment import payment_router
+from app.core.config import settings
+from app.services.outbox import run_outbox_publisher
 
-app = FastAPI()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    broker = RabbitBroker(settings.rabbitmq.URL)
+    await broker.connect()
+
+    task = asyncio.create_task(run_outbox_publisher(broker))
+    yield
+    task.cancel()
+    await asyncio.gather(task, return_exceptions=True)
+    await broker.close()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(payment_router, prefix="/api/v1")
 
 if __name__ == "__main__":
