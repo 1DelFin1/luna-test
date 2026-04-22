fc6b18cfab5751500ec1b0ff140881d1d191c167
andrey <defario777@mail.ru>
2026-04-22 11:08:30 +0500

    add payment routers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.api.routers.payment import payment_router
+
 app = FastAPI()
+
+app.include_router(payment_router, prefix="/api/v1")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
---
+++ app/services/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ("PaymentService",)
+
+from app.services.payment import PaymentService
